8511bdab512f26dc07309f50fb0afd52f6bd959e
shal1 <archana.shalu1@gmail.com>
2016-09-29 19:13:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var note1 = {
+    title:'Note one|sha',
+    heading:'Note 1',
+    date:'sept 22',
+    content:` <p>
+                    welcome to my app.How r u?
+                </p>
+                <p>
+                    welcome to my app.How r u?welcome to my app.How r u?welcome to my app.How r u?welcome to my app.How r u?
+                </p>`
+};
+function createTemplate (data){
+  var title = data.title;
+  var date = data.date;
+  var heading = data.heading;
+  var content = data.content;
+
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+       <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">home</a>
+                
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+             </div>
+             </div>
+             </body>
+</html>`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/note-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'note-one.html'));  
+  res.send(createTemplate(note1));  
 });
 
 app.get('/note-two',function(req,res){
