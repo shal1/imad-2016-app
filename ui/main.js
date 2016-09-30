88a99a16161bbbd6123e2b0da381cef19bc85d68
shal1 <archana.shalu1@gmail.com>
2016-09-30 13:21:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,4 @@
-console.log('Loaded!');
 
-var element = document.getElementById('main-text');
-element.innerHTML = 'Hello and welcome';
-
-var img = document.getElementById('madi');
-var marginRight = 0;
-function moveLeft (){
-    marginRight = marginRight + 3;
-    img.style.marginRight = marginRight + 'px';
-}
-img.onclick=function() {
-    var interval = setInterval(moveLeft,50);
-    
-};
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
