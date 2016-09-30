d0cc041a09ae3da6b746e99bfbd9a0ce56c4eb78
shal1 <archana.shalu1@gmail.com>
2016-09-30 12:24:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'Hello and welcome';
+
+var img = document.getElementById('dino');
+img.onclick=function() {
+    img.style.marginRight = '80 px';
+};
