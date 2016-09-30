9a41c5f276e80642ae9836f53facb1eb345ea637
shal1 <archana.shalu1@gmail.com>
2016-09-30 13:17:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,10 @@ img.onclick=function() {
     var interval = setInterval(moveLeft,50);
     
 };
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    counter= counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML =counter.toString();
+};
