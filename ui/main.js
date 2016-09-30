87a3e2de7e4202335d9dbdb78255b590dcefe6e4
shal1 <archana.shalu1@gmail.com>
2016-09-30 12:34:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var img = document.getElementById('madi');
 var marginRight = 0;
 function moveLeft (){
     marginRight = marginRight + 10;
-    img.style.marginRight = marginRight + px;
+    img.style.marginRight = marginRight + 'px';
 }
 img.onclick=function() {
     var interval = setInterval(moveLeft,200);
