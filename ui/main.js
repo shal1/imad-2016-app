2b2e21585138c89835a1e9346ae64fb29ccc2fcc
shal1 <archana.shalu1@gmail.com>
2016-09-30 12:33:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Hello and welcome';
 
 var img = document.getElementById('madi');
+var marginRight = 0;
+function moveLeft (){
+    marginRight = marginRight + 10;
+    img.style.marginRight = marginRight + px;
+}
 img.onclick=function() {
-    img.style.marginRight = '80px';
+    var interval = setInterval(moveLeft,200);
+    
 };
