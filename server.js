041c919912a3925f39097010e16847cfdd6c0869
shal1 <archana.shalu1@gmail.com>
2016-11-01 13:29:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/counter',function(req,res){
 
 app.get('/notes/:noteName',function(req,res){
     var noteName = req.params.noteName;
-    pool.query("SELECT * FROM articles WHERE title ='"+ req.params.noteName"'",function(err,result){
+    pool.query("SELECT * FROM articles WHERE title ='"+ req.params.noteName + "'",function(err,result){
         if (err){
             res.status(500).send(err.toString());
         }else {
@@ -121,7 +121,7 @@ app.get('/notes/:noteName',function(req,res){
                 res.send(createTemplate(articlesData)); 
             }
         }
-    })
+    });
    
 });
 
