9a2a5f975c6beb205acc528b82515dedbdc9a6ae
shal1 <archana.shalu1@gmail.com>
2016-11-01 14:06:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,6 +75,12 @@ app.get('/counter',function(req,res){
     counter = counter +1;
     res.send(counter.toString());
 });
+var names=[];
+app.get('/submit-name',function(req,res){
+    var name=res.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
 app.get('/notes/:noteName',function(req,res){
     var noteName = req.params.noteName;
